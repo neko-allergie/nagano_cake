4bb344a196d050dffffd6ba0143986cf9a05b1d0
EC2 Default User <ec2-user@ip-172-31-47-223.ap-northeast-1.compute.internal>
2023-05-17 08:11:52 +0000

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
 # 管理者用
 # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords], git controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
