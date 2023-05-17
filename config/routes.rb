ffe240e73e2230a9ea46922603a55af6e9fc21ce
KoichiEndo323 <waddledee323@gmail.com>
2023-05-17 03:15:02 +0000

    [Add]admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] , controllers:{
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
