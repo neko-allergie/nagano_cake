6282106df314a062fe35866c481736f7be59ca5e
Tracer923 <r.arisaka13@gmail.com>
2023-05-17 03:14:53 +0000

    device-customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  
+  
+  
+  # 顧客用
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
