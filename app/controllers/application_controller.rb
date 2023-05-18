0adf0337a3a1d720ec0b61595a33c7f840b85078
KoichiEndo323 <waddledee323@gmail.com>
2023-05-18 05:11:02 +0000

    adminadd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+
+  private
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+        admin_orders_path
+      else
+        customers_path
+      end
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+      if  resource_or_scope == :admin
+        new_admin_session_path
+      else
+        root_path
+      end
+  end
+
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-   email: 'test@gmail.com',
-   password: '123456',
+   email: 'admin@gmail.com',
+   password: 'adminpass',
 )
