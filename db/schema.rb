b341f75e16e6d7123c1cc093382164f2986cc9d4
KoichiEndo323 <waddledee323@gmail.com>
2023-05-19 07:25:53 +0000

    [Add]商品登録画面

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2023_05_17_073800) do
-
+ActiveRecord::Schema.define(version: 2023_05_19_025201) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,7 +74,19 @@ ActiveRecord::Schema.define(version: 2023_05_17_073800) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.string "introduction", null: false
+    t.integer "without_tax", null: false
+    t.integer "sale_status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -92,4 +102,4 @@ ActiveRecord::Schema.define(version: 2023_05_17_073800) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
+end
