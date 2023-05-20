c69b38d918fe234d02a911cacd3d81887c120eda
Tracer923 <r.arisaka13@gmail.com>
2023-05-20 15:02:02 +0000

    add views sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2023_05_19_025201) do
+ActiveRecord::Schema.define(version: 2023_05_19_101622) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
