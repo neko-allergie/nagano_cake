35579ea88b277fa5d16cca511a13c13ccbc9d98e
KoichiEndo323 <waddledee323@gmail.com>
2023-05-22 03:07:50 +0000

    Save changes before switching branches

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,19 +1,36 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
+    @orders = current_customer.orders.latest
   end
 
   def show
+    @order = current_customer.orders.find(params[:id])
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
   end
 
   def new
+    @order = Order.new
   end
 
   def confirm
+
   end
 
   def complete
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id,:adress,:paymethod,:postage,:postcode,:charge)
+  end
+
 end
+
