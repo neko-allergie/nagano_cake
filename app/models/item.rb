481bd9e4fc7ae9c347015b0814d9085f6ed14093
Tracer923 <r.arisaka13@gmail.com>
2023-05-22 06:53:09 +0000

    public items

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,6 +18,6 @@ class Item < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/naganocake.png')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    image.variant(resize_to_limit: [width, height], gravity: "center" ,crop: "125x125+0+0" ).processed
   end
 end
