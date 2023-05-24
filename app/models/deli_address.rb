e004390d734dc80e22f986339c14f8eb8be305d4
shiitakedaisuki <129662461+shiitakedaisuki@users.noreply.github.com>
2023-05-24 02:11:22 +0000

    AddDeli_Addresses

---
+++ app/models/deli_address.rb
@@ -0,0 +1,3 @@
+class DeliAddress < ApplicationRecord
+  belongs_to :customer
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_22_031918) do
+ActiveRecord::Schema.define(version: 2023_05_23_092225) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,15 @@ ActiveRecord::Schema.define(version: 2023_05_22_031918) do
     t.boolean "is_deleted"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "deli_addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "postcode"
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "genres", force: :cascade do |t|
