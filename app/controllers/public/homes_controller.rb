37a3329c5b5994f8489c854c2808e677eecb3938
Tracer923 <r.arisaka13@gmail.com>
2023-05-28 01:08:31 +0000

    layout

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,16 @@
 class Public::HomesController < ApplicationController
     def top
         @genres = Genre.all
+        @items = Item.page(params[:page]).limit(3)
     end
 
     def about
+    end
+    
+    
+      private
+  
+    def item_params
+      params.require(:item).permit(:name,:image,:introduction)
     end
 end
