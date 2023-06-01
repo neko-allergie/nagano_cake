aa575e22c8a54dcc72562312efd625d064d7d67e
Tracer923 <r.arisaka13@gmail.com>
2023-06-01 02:09:55 +0000

    add all

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,14 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+//import "channels"
+import "jquery";
+import "popper.js";
 import "bootstrap";
-import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/js/all';
 import '../stylesheets/application';
 import '../stylesheets/complete';
+//import "script.js"
 // import Swiper from 'swiper/swiper-bundle.js';
 // import 'swiper/swiper-bundle.css';
 
@@ -19,5 +22,3 @@ import '../stylesheets/complete';
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
